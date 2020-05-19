a93239457141faeb89a251cee95f373e9250939d
Lisa Gillfrost <smartalisa@hotmail.com>
2020-05-19 19:50:40 +0200

    init commit

---
+++ CheerApp/CheerAppController.swift
@@ -0,0 +1,9 @@
+//
+//  CheerAppController.swift
+//  CheerApp
+//
+//  Created by Lisa on 2020-05-19.
+//  Copyright © 2020 Lisa. All rights reserved.
+//
+
+import Foundation
--- CheerApp/ViewController.swift
+++ CheerApp/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let context = CIContext()
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func chooseCat() {
+        let catImage = UIImagePickerController()
+        catImage.delegate = self
     }
-
 
 }
 
